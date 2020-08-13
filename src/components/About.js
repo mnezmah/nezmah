b4658261c4e0220ca1226614aa5ae8e80145b869
mnezmah <mario.nezmah@gmail.com>
2020-08-13 20:23:19 +0200

    Upload image

--- src/components/About.js
+++ src/components/About.js
@@ -14,6 +14,9 @@ export default function About() {
               I am a web enthusiast. Anything related to designing, building and running web content is my passion. I put a lot of effort into my work always trying to get a great visual and functional result.</p>
             <p>   &#8221; Make your life a masterpiece; imagine no limitations on what you can be, have&nbsp;or&nbsp;do.&#8221;</p>
           </div>
+          <div className="col-2">
+            <img src="" alt="mario nezmah portrait" className="about__img"/>
+          </div>
         </div>
       </div>
     </div>

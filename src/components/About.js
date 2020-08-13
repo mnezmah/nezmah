def5c2cd639f0526d38f5c3aec5a4b36bfcb3157
mnezmah <mario.nezmah@gmail.com>
2020-08-13 20:53:43 +0200

    Add subtitle scss

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,17 @@ import  portrait from '../Assets/Images/mario_nezmah.png'
 
 export default function About() {
   return (
-    <div id="About">
+    <div id="About" className="mx-5">
       <Title content='Who am i'></Title>
       <div className="about__container">
         <Subtitle content="About Me"></Subtitle>
-        <div className="row">
-          <div className="col-8">
+        <div className="row px-5">
+          <div className="col-8 px-3">
             <p className="about__content">
               I am a web enthusiast. Anything related to designing, building and running web content is my passion. I put a lot of effort into my work always trying to get a great visual and functional result.</p>
-            <p>   &#8221; Make your life a masterpiece; imagine no limitations on what you can be, have&nbsp;or&nbsp;do.&#8221;</p>
+            <p className="about__content">   &#8221; Make your life a masterpiece; imagine no limitations on what you can be, have&nbsp;or&nbsp;do.&#8221;</p>
           </div>
-          <div className="col-2">
+          <div className="col-4 justify-content-center">
             <img src={portrait} alt="mario nezmah portrait" className="about__img"/>
           </div>
         </div>
